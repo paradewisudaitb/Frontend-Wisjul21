1f0d1601988cc78f8b208f8aa811fa4c135098f2
FelineJTD <16520335@std.stei.itb.ac.id>
2021-06-05 19:39:55 +0700

    Home Page Desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Jangan lupa pake Sass buat CSS :D</h1>
-        <h1>Jangan lupa pake bikin branch masing-masing juga :D</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          <a href="https://jspmarc.my.id" target="_blank">Josep</a> was here! :D
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HomePage />
   );
 }
 
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './HomePage.scss';
+import Logo from '/mnt/d/Cave/Wisjul2021/Frontend-Wisjul21/src/images/logo/logo-dark-2020.png';
+import Cloud from '/mnt/d/Cave/Wisjul2021/Frontend-Wisjul21/src/images/bg/cloud.png';
+
+const HomePage = () => {
+
+  return (
+    <>
+      <div className='layers'>
+        <img className='layer-1' src={Cloud} height='300px'/>
+      </div>
+      <div className='container'>
+        <div>
+          <img src={Logo} alt='Logo Wisuda Juli 2021' className='logo' />
+          <h6>Ini tagline Wisjul 2021 Lorem Ipsum Lorem Ipsum</h6>
+        </div>
+        <button className='youtube'>Youtube</button>
+      </div>
+      <div className='visi-misi'>
+        <h1 className='VISI'>VISI</h1>
+        <p className='visi'>visi visi visiii visisisiis viisiss viisisi visisiv isivisiiiv siisiiis viisivis ivisi iivisivi sivsi Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p className='misi'>misi msis sismsi misis msims smis smisms misms msimis smismism smismis smismis ms smsi smsims msims s Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h1 className='MISI'>MISI</h1>
+      </div>
+      <h1>Wisuda Juli 2021</h1>
+      <div className='btns'>
+        <button className='event-btn'>Galeri Wisudawan</button>
+        <button className='event-btn'>Gather Town</button>
+        <button className='event-btn'>Majalah</button>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;

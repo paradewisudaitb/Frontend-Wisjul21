0ffb59a9110b1bfcb5e96a4d5a3074715d00fa0f
Josep Marcello <57025993+jspmarc@users.noreply.github.com>
2021-06-06 00:30:13 +0700

    First "release" (#4)
    * init devel branch
    
    * Feat: add linting to CI script (#1)
    
    * Revert "Feat: add linting to CI script (#1)" (#2)
    
    This reverts commit ab3e646ea35082a304e5305748a73ca1321b898f.
    
    * Feat: CI lint (#3)
    
    * add linting to CI script
    
    * fix: commit sblmnya ada yg lupa diganti
    
    * chore: split lint job and build/deploy job
    
    * feat: update (temporary) home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={'https://wisjul21.sgp1.cdn.digitaloceanspaces.com/logo/logo/Logo-sementara-400x400.jpg'} className="App-logo" alt="logo" />
+      <h1>
+        Wisjul 2021 is coming!
+      </h1>
+      <h2>
+        Stay tuned!
+      </h2>
+      <a
+        href="https://www.instagram.com/paradewisudaitb/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="App-link" >
+        Instagram
+      </a>
+      <br />
+      <a
+        href="https://twitter.com/paradewisudaitb"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="App-link" >
+        Twitter
+      </a>
+      <br />
+      <a
+        href="https://bit.ly/Phoenix1Wisjul"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="App-link" >
+        PHOENIX: Sebongkah Asa
+      </a>
     </div>
   );
 }

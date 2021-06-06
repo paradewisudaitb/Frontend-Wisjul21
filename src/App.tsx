ab642ddbc284b278de2024c2a3d2ed535c54599f
Jeremia Axel <57858415+jeremiaaxel@users.noreply.github.com>
2021-06-06 21:15:56 +0700

    Gabungin yang udah ada dan tambah Navbar + Footer (#5)
    * Home Page Desktop
    
    * Home Page Desktop
    
    * form beres, kurang pesan error
    
    * progess navbar
    
    * ganti jadi typescript, linting
    
    * fix: positioning form container
    
    * add footer
    
    Co-authored-by: FelineJTD <16520335@std.stei.itb.ac.id>
    Co-authored-by: Ariya Adinatha <13519048@std.stei.itb.ac.id>

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import './App.css';
+import HomePage from './pages/HomePage';
+import { DummyPage } from './pages/DummyPage';
+
+import { Footer } from './component/Footer';
+import { Navbar } from './component/Navbar';
+
+import { Route } from 'wouter';
+import { AllRoutes } from './routes/routes';
+
+import Form from './pages/Form/Form';
+
 
 function App() {
   return (
     <div className="App">
-      <img src={'https://wisjul21.sgp1.cdn.digitaloceanspaces.com/logo/logo/Logo-sementara-400x400.jpg'} className="App-logo" alt="logo" />
-      <h1>
-        Wisjul 2021 is coming!
-      </h1>
-      <h2>
-        Stay tuned!
-      </h2>
-      <a
-        href="https://www.instagram.com/paradewisudaitb/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="App-link" >
-        Instagram
-      </a>
-      <br />
-      <a
-        href="https://twitter.com/paradewisudaitb"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="App-link" >
-        Twitter
-      </a>
-      <br />
-      <a
+      {/* { AllRoutes.map(({ path, component }) => (
+        <Route
+          path = { path }
+        >
+          { component }
+        </Route>
+      )) } */}
+      <Route path=''><HomePage /></Route>
+      <Route path='/dummy'><DummyPage /></Route>
+      <Route path='/form'><Form /></Route>
+      
+      {/* Barangkali linknya dibutuhin ntar */}
+      {/* <a
         href="https://bit.ly/Phoenix1Wisjul"
         target="_blank"
         rel="noopener noreferrer"
         className="App-link" >
         PHOENIX: Sebongkah Asa
-      </a>
+      </a> */}
     </div>
   );
 }

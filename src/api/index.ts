46dc45d5c392ee67358bd33b5fbdcd58c1ddd62b
Josep Marcello <jspmarcello@live.com>
2021-06-07 07:56:25 +0700

    change BE URL

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-const API_PROD_URL = 'htttps://wisjulitb.com/api/';
-const API_DEVEL_URL = 'htttp://staging.wisjulitb.com/api/';
+const API_PROD_URL = 'http://backend-prod';
+const API_DEVEL_URL = 'http://bakend-dev';
 export const ASSET_URL = 'https://wisjul21.sgp1.cdn.digitaloceanspaces.com';
 export const API_URL = API_DEVEL_URL;

cf13f160add753649b7bbfcf4a36c6c732945afb
Josep Marcello <jspmarcello@live.com>
2021-06-14 03:07:46 +0700

    feat: API backend dari env (testing)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import HomePage from './pages/HomePage';
-import { DummyPage } from './pages/DummyPage';
 import { Route } from 'wouter';
 import Form from './pages/Form/Form';
+import { API_URL } from './api';
 
 
 function App() {
+  console.log(API_URL);
+  console.log(process.env);
   return (
     <div className="App">
       <Route path=''><HomePage /></Route>

8c349f605b28e08f2a195edf5839adf6016d95fd
Frederick William <19920150@mahasiswa.itb.ac.id>
2021-06-14 23:48:40 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import HomePage from './pages/HomePage';
+import EventPage from './pages/EventPage';
 import { DummyPage } from './pages/DummyPage';
 import { Route } from 'wouter';
 import Form from './pages/Form/Form';
@@ -8,7 +9,7 @@ import Form from './pages/Form/Form';
 function App() {
   return (
     <div className="App">
-      <Route path=''><HomePage /></Route>
+      <Route path=''><EventPage /></Route>
       <Route path='/form'><Form /></Route>
     </div>
   );

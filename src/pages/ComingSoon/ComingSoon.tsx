2326e82cd7d1bc5da41d5d0778bf2febce0b284f
Josep Marcello <jspmarcello@live.com>
2021-06-17 14:17:30 +0700

    add coming soon page

---
+++ src/pages/ComingSoon/ComingSoon.tsx
@@ -0,0 +1,40 @@
+import './ComingSoon.scss';
+
+const ComingSoon = () => {
+  return (
+    <div className="App-header">
+      <img src={'https://wisjul21.sgp1.cdn.digitaloceanspaces.com/assets/logo/Logo-sementara-400x400.jpg'} className="App-logo" alt="logo" />
+      <h1>
+        Wisjul 2021 is coming!
+      </h1>
+      <h2>
+        Stay tuned!
+      </h2>
+      <a
+        href="https://www.instagram.com/paradewisudaitb/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="App-link" >
+        Instagram
+      </a>
+      <br />
+      <a
+        href="https://twitter.com/paradewisudaitb"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="App-link" >
+        Twitter
+      </a>
+      <br />
+      <a
+        href="https://bit.ly/Phoenix1Wisjul"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="App-link" >
+        PHOENIX: Sebongkah Asa
+      </a>
+    </div>
+  );
+};
+
+export default ComingSoon;

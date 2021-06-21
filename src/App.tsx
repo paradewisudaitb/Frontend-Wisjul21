b56b22ceda07040d3a834a70aa452e51346ef495
Fakhri Nail <59967551+fakhrinail@users.noreply.github.com>
2021-06-21 15:24:59 +0700

    Gathertown (#16)
    * raw gathertown
    
    * add filter hmj
    
    * add card hmj
    
    * button shape and font
    
    * button layout
    
    * active button fixed
    
    * fix button center text
    
    * move ButtonFakultas to components

--- src/App.tsx
+++ src/App.tsx
@@ -13,20 +13,39 @@ import ComingSoon from './pages/ComingSoon/ComingSoon';
 import { KirimPesanPage } from './pages/KirimPesan/KirimPesanPage';
 import { Footer } from './component/NavbarFooter/Footer';
 import { Navbar } from './component/NavbarFooter/Navbar';
-
+import Gathertown from './pages/Gathertown/Gathertown';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Navbar />
-      <Route path=''><HomePage /></Route>
-      <Route path='/coming-soon'><ComingSoon /></Route>
-      <Route path='/form'><Form /></Route>
-      <Route path='/form-apresiasi'><FormApresiasi /></Route>
-      <Route path='/majalah'><Majalah /></Route>
-      <Route path='/galeri-apresiasi'><GaleriApresiasi /></Route>
-      <Route path='/wisudawan'><Wisudawan /></Route>
-      <Route path='/kirim-pesan'><KirimPesanPage /></Route>
+      <Route path=''>
+        <HomePage />
+      </Route>
+      <Route path='/coming-soon'>
+        <ComingSoon />
+      </Route>
+      <Route path='/form'>
+        <Form />
+      </Route>
+      <Route path='/form-apresiasi'>
+        <FormApresiasi />
+      </Route>
+      <Route path='/majalah'>
+        <Majalah />
+      </Route>
+      <Route path='/galeri-apresiasi'>
+        <GaleriApresiasi />
+      </Route>
+      <Route path='/wisudawan'>
+        <Wisudawan />
+      </Route>
+      <Route path='/kirim-pesan'>
+        <KirimPesanPage />
+      </Route>
+      <Route path='/gathertown'>
+        <Gathertown />
+      </Route>
       <Footer />
     </div>
   );

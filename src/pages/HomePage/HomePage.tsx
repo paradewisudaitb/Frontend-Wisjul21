0932be0e97f6abfaf576200ff7f12d711ef93b32
FelineJTD <16520335@std.stei.itb.ac.id>
2021-06-22 00:02:05 +0700

    Added parallax to header

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -30,33 +30,45 @@ const HomePage = () => {
   };
 
   return (
-    <div className="pb-5">
-      {/* Header Background */}
-      <div className='bg mt-5' style={{ backgroundImage: `url(${ASSET_URL}/assets/images/background/header.jpg)`}} >
-        {/* Clouds */}
-        <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/awan%201-01.png`} style={{ transform: `translateY(-${offsetY * 0.6}px)`, zIndex: 6 }} />
-        <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/awan%202-01.png`} style={{ transform: `translateY(-${offsetY * 0.7}px)`, zIndex: 7 }} /> 
-        <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/awan%203-01.png`} style={{ transform: `translateY(-${offsetY * 0.2}px)`, zIndex: 2 }} />
-        <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/awan%204-01.png`} style={{ transform: `translateY(-${offsetY * 0.3}px)`, zIndex: 3 }} />
-        <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/awan%205-01.png`} style={{ transform: `translateY(-${offsetY * 0.4}px)`, zIndex: 4 }} />
-        <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/header/awan%20kanan.png`} style={{ transform: `translateY(-${offsetY * 0.5}px)`, zIndex: 5 }} />
-        <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/header/awan%20kiri.png`} style={{ transform: `translateY(-${offsetY * 0.1}px)`, zIndex: 1 }} />
+    <div className='main pb-5'>
+      {/* THIS IS AN 'I HAVE NO IDEA HOW TO COVER THE WHOLE SCREEN WITH CLOUDS MOMENT, PLS HELP MAKE THIS MORE EFFICIENT */}
+
+      
+      {/* Header */}
+      <div className='bg-container mt-5'>
         {/* Volcano */}
-        <img className='smoke' alt='Asap' src={`${ASSET_URL}/assets/images/vistock/header/asap.png`} />
-        <img className='volcanoes' alt='Gunung Berapi' src={`${ASSET_URL}/assets/images/vistock/header/gunung%20pinggir.png`} />
-        <img className='volcano' alt='Gunung Berapi Meletus' src={`${ASSET_URL}/assets/images/vistock/header/gunung%20tengah.png`} />
+        <div className='bg' style={{ backgroundImage: `url(${ASSET_URL}/assets/images/background/header.jpg)`, transform: `translateY(-${offsetY * 0.1}px)`, opacity: `${1-(offsetY/window.outerHeight)*1.7}` }}></div>
+        <img className='smoke' alt='Asap' src={`${ASSET_URL}/assets/images/vistock/header/asap.png`} style={{ transform: `translateY(-${offsetY * 0.5}px)`, opacity: `${1-(offsetY/window.outerHeight)*2}` }} />
+        <img className='volcanoes' alt='Gunung Berapi' src={`${ASSET_URL}/assets/images/vistock/header/gunung%20pinggir.png`} style={{ transform: `translateY(-${offsetY * 0.1}px)`, opacity: `${1-(offsetY/window.outerHeight)*1.2}` }} />
+        <img className='volcano' alt='Gunung Berapi Meletus' src={`${ASSET_URL}/assets/images/vistock/header/gunung%20tengah.png`} style={{ transform: `translateY(-${offsetY * 0.4}px)`, opacity: `${1-(offsetY/window.outerHeight)*1.7}` }} />
       </div>
 
       {/* Header Content */}
-      <div className='home-container pt-5'>
+      <div className='tagline'>
+        <h1 className='tagline1'>Metamorphose to find</h1>
+        <h1 className='tagline2'>the path to the blossom</h1>
+      </div>
+      <div className='home-container pt-5' style={{ transform: `translateY(-${offsetY * 0.6}px)`, opacity: `${1-(offsetY/window.outerHeight)}` }}>
         <div className='logotagline'>
           <img src={`${ASSET_URL}/assets/logo/min.png`} alt='Logo Wisuda Juli 2021' className='logo' />
-          <h5 className='tagline'>Metamorphose to find the path to the blossom.</h5>
+          {/*<h5 className='tagline'>Metamorphose to find the path to the blossom.</h5>*/}
         </div>
         <button className='youtube mx-auto'>Youtube</button>
       </div>
 
-      <div className='main-container' style={{ backgroundImage: `url(${ASSET_URL}/assets/images/background/main.jpg)`}}>
+      <div className='main-container'>
+
+        {/* Clouds */}
+        <div className='cloud-container1'>
+          <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/awan%201-01.png`} style={{ transform: `translateY(-${(offsetY * 0.6)}px)`, zIndex: 6 }} />
+          <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/awan%202-01.png`} style={{ transform: `translateY(-${(offsetY * 0.7)}px)`, zIndex: 7 }} /> 
+          <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/awan%203-01.png`} style={{ transform: `translateY(-${(offsetY * 0.2)}px)`, zIndex: 2 }} />
+          <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/awan%204-01.png`} style={{ transform: `translateY(-${(offsetY * 0.3)}px)`, zIndex: 3 }} />
+          <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/awan%205-01.png`} style={{ transform: `translateY(-${(offsetY * 0.4)}px)`, zIndex: 4 }} />
+          <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/header/awan%20kanan.png`} style={{ transform: `translateY(-${(offsetY * 0.5)}px)`, zIndex: 5 }} />
+          <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/header/awan%20kiri.png`} style={{ transform: `translateY(-${(offsetY * 0.1)}px)`, zIndex: 1 }} />
+        </div>
+
         {/* Visi-Misi */}
         <div className='visi-misi'>
           <h1 className='VISI'>VISI</h1>
@@ -64,6 +76,8 @@ const HomePage = () => {
           <h1 className='MISI'>MISI</h1>
           <p className='misi'>misi msis sismsi misis msims smis smisms misms msimis smismism smismis smismis ms smsi smsims msims s Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
+
+        
 
         {/* Buttons */}
         <h1>Wisuda Juli 2021</h1>

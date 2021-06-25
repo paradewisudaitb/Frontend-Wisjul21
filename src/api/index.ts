7059e3f7369ec55c684093074afd009a8008bfd4
Jeremia Axel <57858415+jeremiaaxel@users.noreply.github.com>
2021-06-25 23:43:58 +0700

    Form apresiasi kelar yey :D (#28)
    * update form apresiasi; max 3 type, batalable
    
    * finish form apresiasi; yey :D

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 export const ASSET_URL = 'https://wisjul21.sgp1.cdn.digitaloceanspaces.com';
+
 // export const API_URL = 'http://localhost:8080';
 export const API_URL = 'http://staging.wisjulitb.com/api';
 // export const API_URL = '/api';

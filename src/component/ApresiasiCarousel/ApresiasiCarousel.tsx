55b88cb8cc302df171d04b54c04274e68a1cc29a
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-07-03 01:03:37 +0700

    delete library

--- src/component/ApresiasiCarousel/ApresiasiCarousel.tsx
+++ src/component/ApresiasiCarousel/ApresiasiCarousel.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Apresiasi } from '../WisudawanCard/Interface';
 import { ASSET_URL } from '../../api';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';

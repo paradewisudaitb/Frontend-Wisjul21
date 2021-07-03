7439da3092b7091c5c0d8ebae2858489c0b895e2
mhmmdjafarg <mjafarashadiq712@gmail.com>
2021-07-03 23:44:42 +0700

    add bg component

--- src/pages/Event/EventPage.tsx
+++ src/pages/Event/EventPage.tsx
@@ -13,26 +13,33 @@ const EventPage = () => {
   const comingSoonEvent = findUpcomingEvent(EVENTS)?.title;
   return (
     <div className="event-page">
-      <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/awan%20matahari-01.png`}  />
-      <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/awan%205-01.png`}  />
-      <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/awan%203-01.png`}  />
+      <img className='bg-component cloud-sun' src={`${ASSET_URL}/assets/images/vistock/main/awan%20matahari-01.png`}  />
+      <img className='bg-component cloud-1' src={`${ASSET_URL}/assets/images/vistock/main/awan%205-01.png`}  />
+      <img className="bg-component cloud-2" src={`${ASSET_URL}/assets/images/vistock/main/awan%202-01.png`}/>
+      <img className="bg-component spark" src={`${ASSET_URL}/assets/images/vistock/main/spark%201%20kanan%20atas-01.png`} />
+      <img className='bg-component bulu-1' src={`${ASSET_URL}/assets/images/vistock/main/bulu.png`}  />
+      <img className='bg-component bulu-2' src={`${ASSET_URL}/assets/images/vistock/main/bulu.png`}  />
+      <img className='bg-component spark-sun' src={`${ASSET_URL}/assets/images/vistock/main/spark%202%20atas%20matahari.png`} />
+      <img className='bg-component awan-203' src={`${ASSET_URL}/assets/images/vistock/main/awan%203-01.png`}  />
+      <img className='bg-component awan-204' src={`${ASSET_URL}/assets/images/vistock/main/awan%204-01.png`}  />
+      {/* <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/awan%203-01.png`}  />
       <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/awan%204-01.png`}  />
       <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/spark%201%20bawah%20matahari.png`}  />
       <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/spark%202%20atas%20matahari.png`}  />
-      <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/bulu.png`}  />
-      <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/bulu.png`}  />
-
-      <h1>What's On Wisjul</h1>
-      <p>
-        Check our instagram, {' '}
-        <strong>
-          <a href="https://www.instagram.com/paradewisudaitb/">
-            @paradewisudaitb
-          </a>
-        </strong>{' '}
-        for futher information.
-      </p>
-      <table>
+      <img className='cloud' src={`${ASSET_URL}/assets/images/vistock/main/bulu.png`}  /> */}
+      <div className="content-header">
+        <h1>What's On Wisjul</h1>
+        <p>
+          Check our instagram, {' '}
+          <strong>
+            <a href="https://www.instagram.com/paradewisudaitb/">
+              @paradewisudaitb
+            </a>
+          </strong>{' '}
+          for futher information.
+        </p>
+      </div>
+      <table className="content-body">
         <tbody className="ukurantabel">
           {EVENTS.map((row, idx) => (
             <Event

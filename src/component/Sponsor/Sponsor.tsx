b69a49f49cd4590c3b68f111b1c5f05f11c36251
shafiranaya <shfrnaya@gmail.com>
2021-07-06 12:35:33 +0700

    ngelist link sponsor

--- src/component/Sponsor/Sponsor.tsx
+++ src/component/Sponsor/Sponsor.tsx
@@ -41,9 +41,12 @@ const medpars = [
 ];
 
 const sponsors = [
-  `${SPONSOR_URL}/a`,
-  `${SPONSOR_URL}/b`,
-  `${SPONSOR_URL}/c`,
+  `${SPONSOR_URL}/XL_Chocolatos.png`,
+  `${SPONSOR_URL}/XL_Paragon.png`,
+  `${SPONSOR_URL}/L_Semen Gresik.png`,
+  `${SPONSOR_URL}/M_Pahamify.jpeg`,
+  `${SPONSOR_URL}/M_PDAM Tirta Perwitasari .JPG`,
+  `${SPONSOR_URL}/S_Cakap.png`
 ];
 
 const Sponsor = () => {

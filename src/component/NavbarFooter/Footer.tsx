326cefa24e57c9b70417dade6c146a274a075caa
Jeremia Axel <57858415+jeremiaaxel@users.noreply.github.com>
2021-07-07 12:49:36 +0700

    Sosmed (#48)
    * Added sosmed
    
    * chore: apus2in spasi
    
    * relayout sosmed in phone
    
    * unlebay burgerbutton
    
    Co-authored-by: FelineJTD <16520335@std.stei.itb.ac.id>
    Co-authored-by: Josep Marcello <jspmarcello@live.com>

--- src/component/NavbarFooter/Footer.tsx
+++ src/component/NavbarFooter/Footer.tsx
@@ -4,47 +4,65 @@ import { Brand } from './Navbar';
 
 import { ReactComponent as InstagramIcon } from '../../icons/instagram.svg';
 import { ReactComponent as TwitterIcon } from '../../icons/twitter.svg';
+import { ReactComponent as TiktokIcon } from '../../icons/tik-tok.svg';
 import { ReactComponent as SpotifyIcon } from '../../icons/spotify.svg';
+import { ReactComponent as LineIcon } from '../../icons/line.svg';
+import { ReactComponent as LinkedinIcon } from '../../icons/linkedin.svg';
 
 import './Footer.scss';
 
 export const Footer = (): JSX.Element => {
   return (
     <footer className="footer">
-      <div className="d-flex justify-content-between">
-        <div className="left-footer ms-4 mt-3">
-          <div className="d-flex flex-row">
-            <Brand />
-          </div>
-          <div className="d-flex flex-row fst-italic">
-            Metamorphose to find the path to the blossom
-          </div>
-          <div className="d-flex flex-row copyright">
-            © Perayaan Wisuda Juli ITB 2021
-          </div>
+      <div className="left-footer">
+        <div className="footer-brand">
+          <Brand />
         </div>
-        <div className="social-media-sec">
-          <a href="https://www.instagram.com/paradewisudaitb/" 
-            target="_blank" 
-            rel="noopener noreferrer"
-            className="social-media-container d-flex instagram">
-            <InstagramIcon className='social-media' />
-          </a>
-          <a href="https://twitter.com/paradewisudaitb" 
-            target="_blank" 
-            rel="noopener noreferrer"
-            className="social-media-container d-flex twitter">
-            <TwitterIcon className='social-media' />
-          </a>
-          <a href="https://bit.ly/Phoenix1Wisjul" 
-            target="_blank" 
-            rel="noopener noreferrer"
-            className="social-media-container d-flex spotify">
-            <SpotifyIcon className='social-media' />
-          </a>
+        <div className="footer-tagline">
+          Metamorphose to find the path to the blossom
+        </div>
+        <div className="footer-copyright">
+          © Perayaan Wisuda Juli ITB 2021
         </div>
       </div>
-        
+      <div className="social-media-sec">
+        <a href="https://www.instagram.com/paradewisudaitb/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-media-container d-flex instagram">
+          <InstagramIcon className='social-media' />
+        </a>
+        <a href="https://twitter.com/paradewisudaitb"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-media-container d-flex twitter">
+          <TwitterIcon className='social-media' />
+        </a>
+        <a href="https://vt.tiktok.com/ZSJmyETDr/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-media-container d-flex tiktok">
+          <TiktokIcon className='social-media' />
+        </a>
+        <a href="https://open.spotify.com/user/huto1ycscch4bkj3fukhzssrr?si=UsvZwc51QGiSPgkjilCVWQ"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-media-container d-flex spotify">
+          <SpotifyIcon className='social-media' />
+        </a>
+        <a href="https://line.me/R/ti/p/%40mov0891c"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-media-container d-flex line">
+          <LineIcon className='social-media' />
+        </a>
+        <a href="https://www.linkedin.com/company/parade-wisuda-juli-itb-2021"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-media-container d-flex linkedin">
+          <LinkedinIcon className='social-media' />
+        </a>
+      </div>
     </footer>
   );
 };

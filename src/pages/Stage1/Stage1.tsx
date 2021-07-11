86a630cc28fd611dcc792f36e63cebc3af748446
fakhrinail <fakhrinailwbw@gmail.com>
2021-07-11 15:58:53 +0700

    add stage 1

---
+++ src/pages/Stage1/Stage1.tsx
@@ -0,0 +1,21 @@
+import './Stage1.scss';
+
+const Stage1 = () => {
+  return (
+    <div className='page-container'>
+      <div>
+        <h2>Congratulations!</h2>
+      </div>
+      <div>
+        <img alt='Gambar stage 1' />
+      </div>
+      <div>
+        <h2>Take Picture</h2>
+        <img alt='Kamera' />
+        <img alt='Pintu keluar' />
+      </div>
+    </div>
+  );
+};
+
+export default Stage1;

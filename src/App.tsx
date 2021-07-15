841b137f5f7116e30d6c26aeeb8f3ec3c554550d
Josep Marcello <jspmarcello@live.com>
2021-07-16 02:55:52 +0700

    fix routing for deployment at Ditmawa's server

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 import './App.scss';
-import { Route, Switch } from 'wouter';
+import { Router, Route, Switch } from 'wouter';
 import { Footer } from './component/NavbarFooter/Footer';
 import { AllRoutes } from './routes/routes';
 import { Loading } from './component/Loading/Loading';
@@ -9,29 +9,32 @@ import { ScrollToTop } from './component/ScrollToTop/ScrollToTop';
 function App(): JSX.Element {
   return (
     <div className='App'>
-      <Suspense fallback={<Loading />}>
-        <ScrollToTop>
+      <Router base='/wisjul2021'>
+        <Suspense fallback={<Loading />}>
+          <ScrollToTop>
 
-          {/* { isHomePage ? <Navbar homePage={true} /> : <Navbar />} */}
-          {/* <Navbar /> */}
-          <div className="app-content">
+            {/* { isHomePage ? <Navbar homePage={true} /> : <Navbar />} */}
+            {/* <Navbar /> */}
+            <div className="app-content">
 
-            <Switch>
-              { AllRoutes.map(({ label, path, component: Component}, props) => (
-                <Route
-                  key={label}
-                  path={path}
-                  component={Component}
-                  {...props}
-                />
-              ))}
-            </Switch>
-          </div>
-        </ScrollToTop>
-        <Footer />
-      </Suspense>
+              <Switch>
+                { AllRoutes.map(({ label, path, component: Component}, props) => (
+                  <Route
+                    key={label}
+                    path={path}
+                    component={Component}
+                    {...props}
+                  />
+                ))}
+              </Switch>
+            </div>
+          </ScrollToTop>
+          <Footer />
+        </Suspense>
+      </Router>
     </div>
   );
 }
 
 export default App;
+

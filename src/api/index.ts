f70befaca4c17cbacc95a9c4bda8a686725270ee
Josep Marcello <jspmarcello@live.com>
2021-07-16 21:53:16 +0700

    update API for ditmawa's server

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
 export const ASSET_URL = 'https://wisjul21.sgp1.cdn.digitaloceanspaces.com';
 
 // export const API_URL = 'http://localhost:8080';
-export const API_URL = '/api';
+export const API_URL = '/wisjul2021/api';
